449f70a04c0d4ff230e3bf8665f5c2279b23be86
Benjamin Rokseth <bensinober@gmail.com>
2017-03-23 22:37:36 +0100

    Use ENV pw in virtuoso.sh

--- virtuoso.sh
+++ virtuoso.sh
@@ -39,8 +39,8 @@ then
   touch /sql-query.sql
   if [ "$DBA_PASSWORD" ]; then echo "user_set_password('dba', '$DBA_PASSWORD');" >> /sql-query.sql ; fi
   if [ "$SPARQL_UPDATE" = "true" ]; then echo "GRANT SPARQL_UPDATE to \"SPARQL\";" >> /sql-query.sql ; fi
-  virtuoso-t +wait && isql-v -U dba -P dba < /dump_nquads_procedure.sql && isql-v -U dba -P dba < /sql-query.sql
-  kill "$(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')"
+  virtuoso-t +wait && isql-v -U dba -P "$DBA_PASSWORD" < /dump_nquads_procedure.sql && isql-v -U dba -P dba < /sql-query.sql
+  kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
   echo "`date +%Y-%m-%dT%H:%M:%S%:z`" >  .dba_pwd_set
 fi
 
@@ -57,7 +57,7 @@ then
     echo "exec('checkpoint');" >> /load_data.sql
     echo "WAIT_FOR_CHILDREN; " >> /load_data.sql
     echo "$(cat /load_data.sql)"
-    virtuoso-t +wait && isql-v -U dba -P "$pwd" < /load_data.sql
+    virtuoso-t +wait && isql-v -U dba -P "$DBA_PASSWORD" < /load_data.sql
     kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
     echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .data_loaded
 fi
